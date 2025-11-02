9cc5b79e3a12fa6f2d9f7432f79d143085af12df
carlospc247 <carlospc247@gmail.com>
2025-11-02 20:57:50 +0100

    f_ff

--- pharmassys/settings.py
+++ pharmassys/settings.py
@@ -237,9 +237,9 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'URL': os.getenv('CLOUDINARY_URL'),
     'SECURE': True,
 }
-
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
